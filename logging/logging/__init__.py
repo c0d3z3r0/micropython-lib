49e63d6d277297b9e8579ac49b726afdf4aa4e71
Michael Niewöhner <foss@mniewoehner.de>
2019-06-26 00:07:36 +0200

    logging: allow brackets format in message

--- logging/logging/__init__.py
+++ logging/logging/__init__.py
@@ -233,7 +233,10 @@ class Formatter:
 
     def format(self, record):
         # The message attribute of the record is computed using msg % args.
-        record.message = record.msg % record.args
+        if self.style == "%" or '%s' in record.msg:
+            record.message = record.msg % record.args
+        elif self.style == "{":
+            record.message = record.msg.format(*record.args)
 
         # If the formatting string contains '(asctime)', formatTime() is called to
         # format the event time.
